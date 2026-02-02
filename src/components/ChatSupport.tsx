fed3cb1e7211ff1536fff7194f342b78663666cd
CongoMusahAdama <musahcongoadama@gmail.com>
2026-02-02 10:36:23 +0000

    fix: syntax error in chat support component

--- src/components/ChatSupport.tsx
+++ src/components/ChatSupport.tsx
@@ -8,7 +8,6 @@ export default function ChatSupport() {
     const { isSupportOpen, setIsSupportOpen } = useCart();
 
     return (
-    return (
         <div className="fixed bottom-6 right-6 z-[50] flex flex-col items-end pointer-events-none">
             {/* Support Box */}
             {isSupportOpen && (
