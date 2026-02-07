a6cfbc4f488b9eb0df44afff8760b295781e7e1f
CongoMusahAdama <musahcongoadama@gmail.com>
2026-02-07 16:17:04 +0000

    fix(build): exclude backend and allow render images

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,10 @@ const nextConfig: NextConfig = {
         protocol: 'http',
         hostname: '127.0.0.1',
         port: '3001',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.onrender.com',
       }
     ],
   },
