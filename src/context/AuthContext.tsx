514a7952798667fe8cd3c1e4af3cda25984cf14e
CongoMusahAdama <musahcongoadama@gmail.com>
2026-05-25 08:19:37 +0000

    update

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -7,6 +7,7 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api';
 export type UserRole = 'customer' | 'admin' | 'vendor';
 
 export type User = {
+    _id?: string;
     id: string;
     name: string;
     email: string;
